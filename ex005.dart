ac9eaa7f6895ea21a817c944659f33f81b88570a
aroldojunior <aroldojunior@brq.com>
2021-01-04 17:21:55 -0300

    Exercicio 005 - sucessor e antecessor

---
+++ ex005.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  print("Digite um numero");
+  int value = int.parse(stdin.readLineSync());
+
+  int antecessor = value - 1;
+  int sucessor = antecessor + 2;
+  // print("este eh o value $value");
+  // print("este eh o antecessor $antecessor");
+  // print("este eh o sucessor $sucessor");
+
+  print(
+      "Analisando o valor $value, seu antecessor é $antecessor e o sucessor é $sucessor");
+}
