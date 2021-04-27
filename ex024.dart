959a112ff3f16b8c388aed619569611659e4ea7b
Aroldo Esmerio <aroldojunior@brq.com>
2021-04-26 21:11:27 -0300

    Exercicio 24 - Verificando as primeiras letras de um texto

---
+++ ex024.dart
@@ -0,0 +1,10 @@
+// Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com a palavra SANTO
+
+import 'dart:io';
+
+void main() {
+  print('Digite a cidade onde vc nasceu ?');
+  String city = stdin.readLineSync().trim().toUpperCase();
+
+  print(city.startsWith('SANTO', 0));
+}
