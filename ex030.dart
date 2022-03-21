f316d687bee4e38a491002a6bf85f29fd98f3ccc
“200dev.aoliveira” <200dev.aoliveira@bv.com.br>
2022-03-21 08:31:03 -0300

    Aula-30 - Exercicio par ou impar

---
+++ ex030.dart
@@ -0,0 +1,16 @@
+/*
+Exercicio 30
+Crie um programa que leia um numero inteiro
+e mostre na tela se é par ou impar
+*/
+
+import 'dart:io';
+
+void main() {
+  print('Digite um número inteiro');
+  int numInt = int.parse(stdin.readLineSync());
+
+  String result = numInt % 2 == 0 ? 'Par' : 'Impar';
+
+  print('=====> $result');
+}
