84e4231c0b4835afddcdba31fd0a544bb063fdf5
“200dev.aoliveira” <200dev.aoliveira@bv.com.br>
2022-07-18 20:41:10 -0300

    Exercício Python #035 - Analisando Triângulo v1.0

---
+++ ex035.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+  print('-' * 10);
+
+  print('Digite o primeiro lado');
+  double v1 = double.parse(stdin.readLineSync());
+  print('Digite o segundo lado');
+  double v2 = double.parse(stdin.readLineSync());
+  print('Digite o terceiro lado');
+  double v3 = double.parse(stdin.readLineSync());
+
+  print('-' * 10);
+  print('Por favor aguarde ... ');
+
+  bool isTriangle = (v1 < v2 + v3) && (v2 < v1 + v3) && (v3 < v1 + v2);
+  Future.delayed(Duration(seconds: 10));
+  print(isTriangle ? 'Triangulo' : 'Não é  Triangulo');
+}
